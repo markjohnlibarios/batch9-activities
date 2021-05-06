7d0a141667607ecf9021c82c9439f45813f72bf6
Mark John Libarios <markjohnlibarios@gmail.com>
2021-05-06 14:32:35 +0800

    additional animation

--- libarios-mark-john/landing-page/assets/js/events.js
+++ libarios-mark-john/landing-page/assets/js/events.js
@@ -12,6 +12,7 @@ function menutoggle(){
 
 /***************************************************** */
 
+/**progress bar**/
 window.onscroll = function() {progressBar()};
 
 function progressBar() {
@@ -19,4 +20,15 @@ function progressBar() {
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("load-bar").style.width = scrolled + "%";
+
+    const header = document.querySelector("header");
+    const progress = document.querySelector("#progress");
+
+    if (scrolled > 10){
+        header.classList.add("nav-scrolled");
+        progress.classList.add("progress-container");
+    } else {
+        header.classList.remove("nav-scrolled");
+        progress.classList.remove("progress-container");
+    }
 }
