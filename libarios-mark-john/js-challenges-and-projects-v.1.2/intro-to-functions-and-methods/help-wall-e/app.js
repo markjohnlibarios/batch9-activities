402512d084ce12bc0b3ac3932a2f6d58c7bacd17
Mark John Libarios <markjohnlibarios@gmail.com>
2021-05-12 21:00:18 +0800

    help-wall-e

--- libarios-mark-john/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/help-wall-e/app.js
+++ libarios-mark-john/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/help-wall-e/app.js
@@ -1,9 +1,37 @@
 const robot = document.querySelector('.robot')
 
 //Challenge: Make Eve move when you click its body.
-
+var counter = 0;
 function moveRobot() {
     //add code here
+    //alert(var1);
+    counter = counter + 10;
+    robot.style.left = counter + "px";
     
 }
-robot.addEventListener('click', moveRobot)
+
+robot.addEventListener('click', moveRobot);
+//robot.addEventListener('click', e => moveRobot(e, 'var1'))
+
+/** prepared for right click event
+ * function moveRobot(e) {
+    //add code here
+    e.preventDefault();
+    counter = counter + 10;
+    if(e.which === 3){
+        robot.style.left = counter + "px";
+        console.log('right' + counter);
+    }
+
+    if(e.which === 1){
+        robot.style.right = counter + "px";
+        console.log('left' + counter);
+    }
+    
+    //robot.style.left = counter + "px";
+    
+}
+
+robot.addEventListener('click', moveRobot);
+robot.addEventListener('contextmenu', moveRobot);
+ * **/
--- libarios-mark-john/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/schools-out/app.js
+++ libarios-mark-john/js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/schools-out/app.js
@@ -1,11 +1,36 @@
-function sendText( ) {
+const student = [
+    {
+        firstname: 'Jose',
+        lastname: 'Rizal'
+    },
+    {
+        firstname: 'Manny',
+        lastname: 'Pacquiao'
+    },
+    {
+        firstname: 'Robert',
+        lastname: 'Rodriguez'
+    },
+    {
+        firstname: 'Elon',
+        lastname: 'Musk'
+    },
+    {
+        firstname: 'Mark',
+        lastname: 'Zuckerberg'
+    },
+];
+function sendText(firstname, lastname ) {
     //Write your code here.
+    console.log("Hi, " + firstname + ' ' + lastname + ' our class for today has been cancelled. Have a safe day');
 }
-sendText()
+//sendText()
 
-
-sendText('Jose', 'Rizal')
+for (let i = 0; i < student.length; i++){
+    sendText(student[i].firstname, student[i].lastname);
+}
+/*sendText('Jose', 'Rizal')
 sendText('Manny', 'Pacquiao')
 sendText('Robert', 'Rodriguez')
 sendText('Elon', 'Musk')
-sendText('Mark', 'Zuckerberg')
+sendText('Mark', 'Zuckerberg')*/
